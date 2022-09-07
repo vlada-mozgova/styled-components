679e2b8197459dc70ab5fb20dbf5bcad10f46298
vlada-mozgova <vladyslava.m@gmail.com>
2022-09-07 11:38:21 +0300

    add global style

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import { Button } from "./common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: "Kaushan Script";
+  }
+`;
 
 const App = () => {
   return (
     <div>
+      <GlobalStyle />
       <h1>App</h1>
       <Button>Hello</Button>
     </div>
